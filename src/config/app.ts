c783ad0c3d87291682aeb9902ad1f1ba7e6dd725
tchamio <fredericgodi@gmail.com>
2026-03-19 01:00:08 +0100

    config for build

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,4 +3,4 @@ export const API_URL_LOCAL = 'http://localhost:5002/api';
 export const APP_NAME = 'MDJ';
 export const APP_VERSION = '1.0.0';
 
-export const ONE_SIGNAL_APP_ID = '27a5725a-0df3-492d-ad4e-4ba5c5b93a52'
+export const ONE_SIGNAL_APP_ID = '27a5725a-0df3-492d-ad4e-4ba5c5b93a52';
